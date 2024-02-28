7570118b60e8a4bef0c4689eaa9460d9c91c57f5
austineamonn <107045458+austineamonn@users.noreply.github.com>
2024-02-27 20:16:36 -0600

    Update whale_pop_model.m
    simplified model, ignore commented out things

--- whale_pop_model.m
+++ whale_pop_model.m
@@ -28,56 +28,56 @@ annprofit=zeros(1,years+1);
 
 %add initial values to their respective vectors
 
-totalpop(1,1)=pop1+pop2;
-bluepop(1,1)=pop1;
-finpop(1,1)=pop2;
+%totalpop(1,1)=pop1+pop2;
+%bluepop(1,1)=pop1;
+%finpop(1,1)=pop2;
 
 %get initial growth and add to growth vectors
 
-growthinit1=whale_growth(pop1,pop2,r1,k1,a1);
-bluegrowth(1,1)=growthinit1;
-growthinit2=whale_growth(pop1,pop2,r2,k2,a2);
-fingrowth(1,1)=growthinit2;
+growth1=whale_growth(pop1,pop2,r1,k1,a1);
+%bluegrowth(1,1)=growthinit1;
+growth2=whale_growth(pop1,pop2,r2,k2,a2);
+%fingrowth(1,1)=growthinit2;
 
 %use type and initial growth to determine hunting level
 %type is the hunting level relative to the growth rate
 %ex: type=1.05 is hunting at 105% of the growth rate
 
-hunt1=type*growthinit1;
-hunt2=type*growthinit2;
+hunt1=type*growth1
+hunt2=type*growth2
 
 %calculate profit
 
-profit1=profit(hunt1,12000,pop1);
-profit2=profit(hunt2,6000,pop2);
-annprofit(1,1)=profit1+profit2;
+%profit1=profit(hunt1,12000,pop1);
+%profit2=profit(hunt2,6000,pop2);
+%annprofit(1,1)=profit1+profit2;
 
-totalprofit=annprofit(1,1); %initialize total profit
+%totalprofit=annprofit(1,1); %initialize total profit
+totalprofit=0;
 
 %calculate new population for year 1
 
-pop1=newpop(pop1,growthinit1,hunt1);
-pop2=newpop(pop2,growthinit2,hunt2);
+%pop1=newpop(pop1,growthinit1,hunt1);
+%pop2=newpop(pop2,growthinit2,hunt2);
 
 %iterate for year 1 to year 'years'
 
-for i=2:years+1
+for i=1:years+1
+    %calculate current population
+    pop1=newpop(pop1,growth1,hunt1,i-1);
+    bluepop(1,i)=pop1;
+    pop2=newpop(pop2,growth2,hunt2,i-1);
+    finpop(1,i)=pop2;
+    totalpop(1,i)=pop1+pop2;
     %calculate annual growth
     growth1=whale_growth(pop1,pop2,r1,k1,a1);
-    growth2=whale_growth(pop1,pop2,r2,k2,a2);
+    growth2=whale_growth(pop2,pop1,r2,k2,a2);
     %update all the output vectors
     bluegrowth(1,i)=growth1;
     fingrowth(1,i)=growth2;
-    pop1=newpop(pop1,growth1,hunt1);
-    bluepop(1,i)=pop1;
-    pop2=newpop(pop2,growth2,hunt2);
-    finpop(1,i)=pop2;
-    totalpop(1,i)=pop1+pop2;
     profit1=profit(hunt1,12000,pop1);
     profit2=profit(hunt2,6000,pop2);
     combprofit=profit1+profit2;
     annprofit(1,i)=combprofit;
     totalprofit=totalprofit+combprofit;
 end
-
-
