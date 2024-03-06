3623c2e7473a95a0707cdea1b674dd86b725a993
Austin Nicolas <107045458+austineamonn@users.noreply.github.com>
2024-03-05 18:37:11 -0600

    Update competition.m
    added comments

--- competition.m
+++ competition.m
@@ -1,7 +1,8 @@
 function [type,comp]=competition(r1,r2,k1,k2,a1,a2)
 
-comp=(r1/k1)*(r2/k2)-a1*a2;
+comp=(r1/k1)*(r2/k2)-a1*a2; %calculate if it is strong or weak competition
 
+%declare type of competition depending on 'comp' value
 if comp>0
     type='weak';
 elseif comp<0
