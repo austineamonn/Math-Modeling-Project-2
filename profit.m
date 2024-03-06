6ff4a4b637bf3afbb55592cf800d007cf9301c71
Austin Nicolas <107045458+austineamonn@users.noreply.github.com>
2024-03-05 18:34:25 -0600

    Update profit.m
    added comments

--- profit.m
+++ profit.m
@@ -1,7 +1,8 @@
 function resprofit=profit(hunt,price,pop)
 
+%calculate profit based on number of whales hunted and the price
 resprofit=hunt*price;
 
-if pop<hunt
+if pop<hunt %prevents negative population by limiting hunting to the number of whales that are left
     resprofit=pop*price;
 end
