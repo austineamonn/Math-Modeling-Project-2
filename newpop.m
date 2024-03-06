8a73d4220a9d1088b37ccaebda44ec5c1d804b73
Austin Nicolas <107045458+austineamonn@users.noreply.github.com>
2024-03-05 18:35:23 -0600

    Update newpop.m
    added comments

--- newpop.m
+++ newpop.m
@@ -3,8 +3,8 @@ function pophat=newpop(pop,growth,hunt,initial)
 if initial==0 %for case where calculating inital population
     pophat=pop;
 else
-    pophat=growth+pop-hunt;
-    if pophat<0
+    pophat=growth+pop-hunt; %new population is the old population + new growth - whales hunted
+    if pophat<0 %prevent negative population
         pophat=0;
     end
 end
