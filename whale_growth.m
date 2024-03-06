8edae1e1592252b617912bc4ae9e2ae539559eab
Austin Nicolas <107045458+austineamonn@users.noreply.github.com>
2024-03-05 18:28:53 -0600

    Update whale_growth.m
    added comments

--- whale_growth.m
+++ whale_growth.m
@@ -1,10 +1,8 @@
 function growth=whale_growth(pop1,pop2,r,k,a)
 
-if nargin<1
+if nargin<1 %give error if no inputs
     error('missing inputs');
 end
 
+%use growth function from IWC
 growth=r*pop1*(1-pop1/k)-a*pop1*pop2;
-
-%1725, 7850
-%1731, 7856
