7eef06fed0201c6f36c0e5c365b1546790f9a549
JimDeng123 <137099265+JimDeng123@users.noreply.github.com>
2024-03-05 22:46:03 -0600

    Delete max_profit.m

--- max_profit.m
+++
@@ -1,4 +0,0 @@
-A = [18*10^(-8) 12*0.1/150000; 6*0.16/400000 18*10^(-8)];
-b = [12*0.05; 6*0.08];
-x = A\b
-%A population of 194703 Fin whales and 70619 Blue whales will result in the maximum revenue
