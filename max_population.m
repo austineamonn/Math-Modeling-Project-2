493e455c8091ea5af23ec4eb6b00e76fe628d17d
JimDeng123 <137099265+JimDeng123@users.noreply.github.com>
2024-03-05 22:45:55 -0600

    Delete max_population.m

--- max_population.m
+++
@@ -1,6 +0,0 @@
-A = [10^(-8) 0.05/150000; 0.08/400000 10^(-8)];
-b = [0.05; 0.08];
-x = A\b
-
-
-%A population of 393,090 Fin whales and 138,207 Blue whales will result in a total of 531,297 whales, 
